8dbc41dedf7dd482278319b9350a01e38a373af6
Julian Bieber <j.bieber@patagona.de>
2022-11-14 11:39:55 +0100

    init

---
+++ go-s3-test.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	// "io"
+	"os"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/request"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+func main() {
+	var bucket, key string
+	var timeout, _ = time.ParseDuration("10s")
+	var upload_path string
+	flag.StringVar(&bucket, "b", "", "Bucket name.")
+	flag.StringVar(&key, "k", "", "Object key name.")
+	flag.StringVar(&upload_path, "p", "", "path to upload")
+	flag.Parse()
+
+	var file, _ = os.Open(upload_path)
+	defer file.Close()
+
+	sess := session.Must(session.NewSession())
+
+	// Create a new instance of the service's client with a Session.
+	// Optional aws.Config values can also be provided as variadic arguments
+	// to the New function. This option allows you to provide service
+	// specific configuration.
+	svc := s3.New(sess)
+
+	// Create a context with a timeout that will abort the upload if it takes
+	// more than the passed in timeout.
+	ctx := context.Background()
+	var cancelFn func()
+	if timeout > 0 {
+		ctx, cancelFn = context.WithTimeout(ctx, timeout)
+	}
+	// Ensure the context is canceled to prevent leaking.
+	// See context package for more information, https://golang.org/pkg/context/
+	if cancelFn != nil {
+		defer cancelFn()
+	}
+
+	// Uploads the object to S3. The Context will interrupt the request if the
+	// timeout expires.
+	_, err := svc.PutObjectWithContext(ctx, &s3.PutObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+		Body:   file,
+	})
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok && aerr.Code() == request.CanceledErrorCode {
+			// If the SDK can determine the request or retry delay was canceled
+			// by a context the CanceledErrorCode error code will be returned.
+			fmt.Fprintf(os.Stderr, "upload canceled due to timeout, %v\n", err)
+		} else {
+			fmt.Fprintf(os.Stderr, "failed to upload object, %v\n", err)
+		}
+		os.Exit(1)
+	}
+
+	fmt.Printf("successfully uploaded file to %s/%s\n", bucket, key)
+}
---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/julianbieber/go-s3-test
+
+go 1.13
+
+require (
+	github.com/aws/aws-sdk-go v1.34.3
+)
